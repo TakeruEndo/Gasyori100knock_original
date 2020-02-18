455afe9cb767760c4bb38824836b3ea2c7bc73ad
takeru <sitentaduro1118@gmail.com>
2020-02-18 16:59:41 +0900

    manage question status

--- management.py
+++ management.py
@@ -1,8 +1,23 @@
 import os
 import glob
 
-path = "./"
-files = glob.glob(path + 'Q1.py', path + 'Q1.py')
 
-for f in files:
-    os.rename(f, os.path.join(path, 'img_' + os.path.basename(f)))
+def add_status(unsolved__list: list, solved_list: list, file_path: str):
+    files = glob.glob(file_path + '*')
+    for i in unsolved__list:
+        for f in files:
+            if f == file_path + f'Q{i}.py':
+                path = file_path + f'Q{i}.py'
+                os.rename(path, os.path.join(file_path, f'Q{i}_not_completed.py'))
+    for i in solved_list:
+        for f in files:
+            if f == file_path + f'Q{i}_not_completed.py':
+                path = file_path + f'Q{i}_not_completed.py'
+                os.rename(path, os.path.join(file_path, f'Q{i}.py'))
+
+
+if __name__ == '__main__':
+    unsolved_num = [5, 8, 9, 10]
+    solved_num = [1, 2, 3, 4]
+    add_status(unsolved_num, solved_num, './Q1-10/')
+
